35ba13a9d9bf9a8346d13dd1534e1eb3eda7d89f
Extraskilled56 <zjones.2012@icloud.com>
2024-09-21 20:37:14 -0500

    update instal script

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
+if [[ $EUID -ne 0 ]]; then 
+
+    echo "This script requires root privileges. Please run with sudo."
+
+    exit 1
+
+fi
+
+
 echo installing
-apt install make cc git
+apt install make gcc git
 cd /tmp
 git clone https://github.com/bit-sync/ops.git
-cd ops
+cd ops/src
 make install
+echo done
